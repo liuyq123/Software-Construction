7e6b6e80c4b744ba20a83957e8ddde7a18c6570f
liuyq123 <517417512@qq.com>
2020-06-17 21:54:46 -0700

    test

---
+++ ps2/test/graph/GraphInstanceTest.java
@@ -0,0 +1,127 @@
+/* Copyright (c) 2015-2016 MIT 6.005 course staff, all rights reserved.
+ * Redistribution of original or derived work requires permission of course staff.
+ */
+package graph;
+
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+import org.junit.Test;
+
+/**
+ * Tests for instance methods of Graph.
+ * 
+ * <p>PS2 instructions: you MUST NOT add constructors, fields, or non-@Test
+ * methods to this class, or change the spec of {@link #emptyInstance()}.
+ * Your tests MUST only obtain Graph instances by calling emptyInstance().
+ * Your tests MUST NOT refer to specific concrete implementations.
+ */
+public abstract class GraphInstanceTest {
+    
+    // Testing strategy
+    //   TODO
+    
+    /**
+     * Overridden by implementation-specific test classes.
+     * 
+     * @return a new empty graph of the particular implementation being tested
+     */
+    public abstract Graph<String> emptyInstance();
+    
+    @Test(expected=AssertionError.class)
+    public void testAssertionsEnabled() {
+        assert false; // make sure assertions are enabled with VM argument: -ea
+    }
+    
+    @Test
+    public void testInitialVerticesEmpty() {
+        // TODO you may use, change, or remove this test
+        assertEquals("expected new graph to have no vertices",
+                Collections.emptySet(), emptyInstance().vertices());
+    }
+
+    @Test
+    public void testAdd() {
+        Graph<String> emptyInstance = emptyInstance();
+
+        assertEquals(true, emptyInstance.add("a"));
+        assertEquals(false, emptyInstance.add("a"));
+        assertEquals(true, emptyInstance.add("b"));
+        assertEquals(true, emptyInstance.add("cd"));
+    }
+
+    @Test
+    public void testSet() {
+        Graph<String> emptyInstance = emptyInstance();
+
+        emptyInstance.add("a");
+        emptyInstance.add("b");
+        emptyInstance.add("c");
+
+        assertEquals(0, emptyInstance.set("a", "b", 1));
+        assertEquals(1, emptyInstance.set("a", "b", 2));
+
+        assertEquals(0, emptyInstance.set("c", "c", 1));
+        assertEquals(0, emptyInstance.set("c", "c", 1));
+
+        assertEquals(Collections.EMPTY_MAP, emptyInstance.sources("c"));
+        assertEquals(Collections.EMPTY_MAP, emptyInstance.targets("c"));
+    }
+
+    @Test
+    public void testRemove() {
+        Graph<String> emptyInstance = emptyInstance();
+
+        emptyInstance.add("a");
+        emptyInstance.add("b");
+
+        assertEquals(true, emptyInstance.remove("b"));
+        assertEquals(false, emptyInstance.remove("b"));
+    }
+
+    @Test
+    public void testVertices() {
+        Graph<String> emptyInstance = emptyInstance();
+
+        emptyInstance.add("a");
+        emptyInstance.add("b");
+        emptyInstance.add("c");
+
+        Set<String> vertices = new HashSet<String>();
+        vertices.add("a");
+        vertices.add("b");
+        vertices.add("c");
+
+        assertEquals(vertices, emptyInstance.vertices());
+    }
+
+    @Test
+    public void testSourceTarget() {
+        Graph<String> emptyInstance = emptyInstance();
+
+        emptyInstance.add("a");
+        emptyInstance.add("b");
+        emptyInstance.add("c");
+        emptyInstance.add("d");
+
+        emptyInstance.set("a", "b", 1);
+        emptyInstance.set("b", "c", 3);
+        emptyInstance.set("a", "d", 5);
+        emptyInstance.set("b", "b", 1);
+        emptyInstance.set("b", "b", 1);
+        emptyInstance.set("b", "c", 2);
+        emptyInstance.set("d", "a", 4);
+
+        Map<String, Integer> targets = new HashMap<String, Integer>();
+        targets.put("b", 1);
+        targets.put("c", 3);
+        targets.put("d", 5);
+        assertEquals(targets, emptyInstance.targets("1"));
+        assertEquals(Collections.EMPTY_MAP, emptyInstance.targets("3"));
+        targets.clear();
+        targets.put("a", 4);
+        assertEquals(targets, emptyInstance.targets("4"));
+    }
+    
+}
---
+++ ps2/test/graph/GraphStaticTest.java
@@ -0,0 +1,51 @@
+/* Copyright (c) 2015-2016 MIT 6.005 course staff, all rights reserved.
+ * Redistribution of original or derived work requires permission of course staff.
+ */
+package graph;
+
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+import org.junit.Test;
+
+/**
+ * Tests for static methods of Graph.
+ * 
+ * To facilitate testing multiple implementations of Graph, instance methods are
+ * tested in GraphInstanceTest.
+ */
+public class GraphStaticTest {
+    
+    // Testing strategy
+    //   empty()
+    //     no inputs, only output is empty graph
+    //     observe with vertices()
+    
+    @Test(expected=AssertionError.class)
+    public void testAssertionsEnabled() {
+        assert false; // make sure assertions are enabled with VM argument: -ea
+    }
+    
+    @Test
+    public void testEmptyVerticesEmpty() {
+        assertEquals("expected empty() graph to have no vertices",
+                Collections.emptySet(), Graph.empty().vertices());
+    }
+
+    @Test
+    public void testOtherType() {
+        Graph<Integer> emptyInstance = Graph.empty();
+        assertTrue(emptyInstance.add(1));
+        assertTrue(emptyInstance.add(2));
+        assertTrue(emptyInstance.add(3));
+        assertEquals(0, emptyInstance.set(2, 1, 4));
+        assertEquals(0, emptyInstance.set(2, 3, 5));
+
+        Map<Integer, Integer> targets = new HashMap<>();
+        targets.put(1, 4);
+        targets.put(3, 5);
+        assertEquals(targets, emptyInstance.targets(2));
+        assertTrue("expect empty map", emptyInstance.targets(3).isEmpty());
+    }
+}
