752ddf6a0650ddcce878b5e07450b51fc91de08e
liuyq123 <517417512@qq.com>
2020-06-18 21:38:13 -0700

    vertices

--- ps2/src/graph/ConcreteVerticesGraph.java
+++ ps2/src/graph/ConcreteVerticesGraph.java
@@ -3,84 +3,207 @@
  */
 package graph;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import edu.princeton.cs.algs4.In;
+
+import java.util.*;
 
 /**
  * An implementation of Graph.
- * 
+ *
  * <p>PS2 instructions: you MUST use the provided rep.
  */
 public class ConcreteVerticesGraph implements Graph<String> {
-    
+
     private final List<Vertex> vertices = new ArrayList<>();
-    
-    // Abstraction function:
-    //   TODO
-    // Representation invariant:
-    //   TODO
-    // Safety from rep exposure:
-    //   TODO
-    
-    // TODO constructor
-    
-    // TODO checkRep
-    
+
     @Override public boolean add(String vertex) {
-        throw new RuntimeException("not implemented");
+        for (Vertex v : vertices) {
+            if (v.getVertex() == vertex)
+                return false;
+        }
+        vertices.add(new Vertex(vertex));
+        return true;
     }
-    
+
     @Override public int set(String source, String target, int weight) {
-        throw new RuntimeException("not implemented");
+        if (weight < 0)
+            throw new RuntimeException("weight cannot be negative");
+
+        if (source == target) {
+            return 0;
+        }
+
+        add(source);
+        Vertex sourceV = null;
+        for (Vertex v : vertices) {
+            if (v.getVertex() == source)
+                sourceV = v;
+        }
+
+        add(target);
+        Vertex targetV = null;
+        for (Vertex v : vertices) {
+            if (v.getVertex() == target)
+                targetV = v;
+        }
+
+        int prevWeight;
+
+        if (weight != 0) {
+            sourceV.addOutEdge(target, weight);
+            prevWeight = targetV.addInEdge(source, weight);
+        } else {
+            targetV.removeInEdge(source);
+            prevWeight = sourceV.removeOutEdge(target);
+        }
+
+        return prevWeight;
     }
-    
+
     @Override public boolean remove(String vertex) {
-        throw new RuntimeException("not implemented");
+        Vertex v = null;
+        for (Vertex ver : vertices) {
+            if (ver.getVertex() == vertex)
+                v = ver;
+        }
+
+        if (v == null)
+            return false;
+
+        Map<String, Integer> out = v.getOutEdges();
+        Map<String, Integer> in = v.getInEdges();
+
+        if (vertices.contains(v)) {
+            for (Map.Entry<String, Integer> outEntry : out.entrySet()) {
+                Vertex o = new Vertex(outEntry.getKey());
+                o.getInEdges().remove(vertex);
+            }
+            for (Map.Entry<String, Integer> inEntry : in.entrySet()) {
+                Vertex i = new Vertex(inEntry.getKey());
+                i.getOutEdges().remove(vertex);
+            }
+        }
+
+        return vertices.remove(v);
     }
-    
+
     @Override public Set<String> vertices() {
-        throw new RuntimeException("not implemented");
+        Set<String> set = new HashSet<>();
+        for (Vertex v : vertices) {
+            set.add(v.getVertex());
+        }
+        return set;
     }
-    
+
     @Override public Map<String, Integer> sources(String target) {
-        throw new RuntimeException("not implemented");
+        for (Vertex s : vertices) {
+            if (s.getVertex() == target);
+            return s.getInEdges();
+        }
+        return null;
     }
-    
+
     @Override public Map<String, Integer> targets(String source) {
-        throw new RuntimeException("not implemented");
+        for (Vertex s : vertices) {
+            if (s.getVertex() == source);
+            return s.getOutEdges();
+        }
+        return null;
     }
-    
+
+
+
     // TODO toString()
-    
+
 }
 
 /**
- * TODO specification
  * Mutable.
  * This class is internal to the rep of ConcreteVerticesGraph.
- * 
+ *
  * <p>PS2 instructions: the specification and implementation of this class is
  * up to you.
  */
 class Vertex {
-    
-    // TODO fields
-    
-    // Abstraction function:
-    //   TODO
-    // Representation invariant:
-    //   TODO
-    // Safety from rep exposure:
-    //   TODO
-    
-    // TODO constructor
-    
-    // TODO checkRep
-    
-    // TODO methods
-    
+
+    private final String vertex;
+    private Map<String, Integer> inEdges = new HashMap<>();
+    private Map<String, Integer> outEdges = new HashMap<>();
+
+    Vertex(String vertex) {
+        this.vertex = vertex;
+    }
+
+    public String getVertex() {
+        return this.vertex;
+    }
+
+    public Map<String, Integer> getInEdges() {
+        Map<String, Integer> sources = new HashMap<>();
+        sources.putAll(inEdges);
+        return sources;
+    }
+
+    public Map<String, Integer> getOutEdges() {
+        Map<String, Integer> targets = new HashMap<>();
+        targets.putAll(outEdges);
+        return targets;
+    }
+
+    // return the weight of the removed edge, or 0 if not exist
+    public int removeInEdge(String source) {
+        if (!inEdges.containsKey(source)) {
+            return 0;
+        }
+
+        int weight = inEdges.remove(source);
+        return weight;
+    }
+
+    public int removeOutEdge(String target) {
+        if (!outEdges.containsKey(target)) {
+            return 0;
+        }
+
+        int weight = outEdges.remove(target);
+        return weight;
+    }
+
+    // return prev weight of the edge
+    public int addInEdge(String source, int weight) {
+        int prevWeight;
+        if (!inEdges.containsKey(source)) {
+            prevWeight = 0;
+        } else {
+            prevWeight = inEdges.get(source);
+            removeInEdge(source);
+        }
+
+        if (weight == 0) {
+            return prevWeight;
+        } else {
+            inEdges.put(source, weight);
+            return prevWeight;
+        }
+    }
+
+    public int addOutEdge(String target, int weight) {
+        int prevWeight;
+        if (!outEdges.containsKey(target)) {
+            prevWeight = 0;
+        } else {
+            prevWeight = outEdges.get(target);
+            removeInEdge(target);
+        }
+
+        if (weight == 0) {
+            return prevWeight;
+        } else {
+            outEdges.put(target, weight);
+            return prevWeight;
+        }
+    }
+
     // TODO toString()
-    
+
 }
--- ps2/test/graph/GraphInstanceTest.java
+++ ps2/test/graph/GraphInstanceTest.java
@@ -11,26 +11,26 @@ import org.junit.Test;
 
 /**
  * Tests for instance methods of Graph.
- * 
+ *
  * <p>PS2 instructions: you MUST NOT add constructors, fields, or non-@Test
  * methods to this class, or change the spec of {@link #emptyInstance()}.
  * Your tests MUST only obtain Graph instances by calling emptyInstance().
  * Your tests MUST NOT refer to specific concrete implementations.
  */
 public abstract class GraphInstanceTest {
-    
+
     /**
      * Overridden by implementation-specific test classes.
-     * 
+     *
      * @return a new empty graph of the particular implementation being tested
      */
     public abstract Graph<String> emptyInstance();
-    
+
     @Test(expected=AssertionError.class)
     public void testAssertionsEnabled() {
         assert false; // make sure assertions are enabled with VM argument: -ea
     }
-    
+
     @Test
     public void testInitialVerticesEmpty() {
         // TODO you may use, change, or remove this test
